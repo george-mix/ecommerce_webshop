782c90aae1b1a22e397ba6c53f97ee1a5de8678c
George <gosha.gosha9696@gmail.com>
2021-02-22 13:26:11 +0300

    basic routes

---
+++ server/controllers/productController.js
@@ -0,0 +1,16 @@
+class ProductController {
+    async create(req, res) {
+
+    }
+
+    async getAll(req, res) {
+
+    }
+
+    async getOne(req, res) {
+
+    }
+
+}
+
+module.exports = new ProductController();
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express');
+const router = new Router;
+const brandRouter = require('./brandRouter');
+const categoryRouter = require('./categoryRouter');
+const productRouter = require('./productRouter');
+const userRouter = require('./userRouter');
+
+router.use('/user', userRouter)
+router.use('/brand', brandRouter)
+router.use('/category', categoryRouter)
+router.use('/product', productRouter)
+
+module.exports = router;
