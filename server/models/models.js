448b57adb9d41b277c5588ff627870bb29bca1fb
George <gosha.gosha9696@gmail.com>
2021-02-22 12:42:00 +0300

    basic models

---
+++ server/models/models.js
@@ -0,0 +1,77 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+const Basket = sequelize.define('basket', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketProduct = sequelize.define('basket_product', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Product = sequelize.define('product', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    price: { type: DataTypes.INTEGER, allowNull: false },
+    img: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Category = sequelize.define('category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false }
+});
+
+const Brand = sequelize.define('brand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false }
+});
+
+const ProductInfo = sequelize.define('product_info', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull: false },
+});
+
+const CategoryBrand = sequelize.define('category_brand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User)
+
+Basket.hasMany(BasketProduct);
+BasketProduct.belongsTo(Basket);
+
+Category.hasMany(Product);
+Product.belongsTo(Category);
+
+Brand.hasMany(Product);
+Product.belongsTo(Brand);
+
+Product.hasMany(BasketProduct);
+BasketProduct.belongsTo(Product);
+
+Product.hasMany(ProductInfo);
+ProductInfo.belongsTo(Product);
+
+Category.belongsToMany(Brand, { through: CategoryBrand });
+Brand.belongsToMany(Category, { through: CategoryBrand });
+
+module.exports = {
+    User,
+    Basket,
+    BasketProduct,
+    Product,
+    Category,
+    Brand,
+    CategoryBrand,
+    ProductInfo
+}
